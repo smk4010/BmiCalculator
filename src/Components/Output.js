1e6b7a8bd23c1d94a3d5a47a9d588fffab0480ee
Scott Kuchinski <skuch@ugao.upenn.edu>
2017-01-18 14:21:32 -0500

    first commit, finished app

---
+++ src/Components/Output.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Output extends Component {
+  toFeet(n){
+      let realFeet = ((n*0.393700) / 12);
+      let feet = Math.floor(realFeet);
+      let inches = Math.round((realFeet - feet) * 12);
+      return feet + "'" + inches;
+  }
+
+  toLbs(n){
+    let nearExact = n/0.45359237;
+    let lbs = Math.floor(nearExact);
+    return lbs;
+  }
+
+  render() {
+    let height = this.toFeet(this.props.data.height);
+    let weight = this.toLbs(this.props.data.weight);
+    let bmi = this.props.data.bmi;
+    let bmiClass = this.props.data.bmiClass;
+
+    return (
+      <div className="output">
+        <h3>{height}</h3>
+        <h3>{weight}lbs</h3>
+        <h3>{bmi}</h3>
+        <h3 className={(this.props.data.bmiClass === "Obese") ? "danger" : ""}>{bmiClass} {(this.props.data.bmiClass === "Obese") ? <a href='http://someproductsite.com'>What Can I do?</a> : ""}</h3>
+      </div>
+    );
+  }
+}
+
+export default Output;
